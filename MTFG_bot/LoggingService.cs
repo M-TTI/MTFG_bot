e7a9cb1b13ee14cf452fa741648f1adb9b7e3d8e
M_TTI <martin.bonetti.dev@gmail.com>
2024-10-09 18:08:38 +0200

    added LoggingService.cs

---
+++ MTFG_bot/LoggingService.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace MTFG_bot;
+
+public class LoggingService
+{
+    public LoggingService(DiscordSocketClient client, CommandService command)
+    {
+        client.Log += LogAsync;
+        command.Log += LogAsync;
+    }
+
+    private Task LogAsync(LogMessage msg)
+    {
+        if (msg.Exception is CommandException cmdException)
+        {
+            Console.WriteLine($"[Command/{msg.Severity}] {cmdException.Command.Aliases.First()}"
+                + $"Failed to execute {cmdException.Context.Channel}.");
+            Console.WriteLine(cmdException);
+        }
+        else
+            Console.WriteLine($"[General/{msg.Severity}] {msg}");
+        return Task.CompletedTask;
+    }
+}
