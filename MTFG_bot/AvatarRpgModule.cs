0652799a629a93a0b8db6472b2ed197034af82d8
Flagg <dont@contact.me>
2024-10-11 17:29:42 +0200

    added emojis for the dice results

--- MTFG_bot/AvatarRpgModule.cs
+++ MTFG_bot/AvatarRpgModule.cs
@@ -1,5 +1,6 @@
 using Discord.Commands;
 using System;
+using System.Collections;
 using System.Threading.Tasks;
 
 
@@ -14,12 +15,25 @@ namespace MTFG_bot {
         [Summary("Rolls two dice.")]
         public async Task RollAsync()
         {
-            // Roll two numbers between 1 and 6
-            int firstRoll = _random.Next(1, 7);
-            int secondRoll = _random.Next(1, 7);
+            var task = ReplyAsync($"{RollDiceEmoji()} {RollDiceEmoji()}");
+            var disposable = Context.Channel.EnterTypingState();
 
-            // Reply with the two dice results
-            await ReplyAsync($"{firstRoll} {secondRoll}");
+            await task;
+            disposable.Dispose();
+        }
+
+        private string RollDiceEmoji() {
+            var nb = _random.Next(1, 7);
+            
+            return nb switch {
+                1 => "<:d6_1:1294307625303019641>",
+                2 => "<:d6_2:1294315102652334080>",
+                3 => "<:d6_3:1294315106649378898>",
+                4 => "<:d6_4:1294315108578754560>",
+                5 => "<:d6_5:1294315110583631882>",
+                6 => "<:d6_6:1294315112500428821>",
+                _ => "❔"
+            };
         }
     }
 }
